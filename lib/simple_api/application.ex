96d1a5d02ab976fd4ed590da53ea7e86c9a120d7
icicle <unknown@icicleling.com>
2021-02-28 22:42:24 +0800

    Add simple api

--- lib/simple_api/application.ex
+++ lib/simple_api/application.ex
@@ -10,6 +10,7 @@ defmodule SimpleApi.Application do
     children = [
       # Starts a worker by calling: SimpleApi.Worker.start_link(arg)
       # {SimpleApi.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: SimpleAPI.Router, options: [port: 8080]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
---
+++ lib/simple_api/router.ex
@@ -0,0 +1,15 @@
+defmodule SimpleAPI.Router do
+  use Plug.Router
+
+  plug(:match)
+  plug(:dispatch)
+
+  get "/do-nothing" do
+    send_resp(conn, 200, "The api does nothing, it's just an api")
+  end
+
+  get "/do-something" do
+    _ = "love you"
+    send_resp(conn, 200, "This api does something, but you don't know it")
+  end
+end
